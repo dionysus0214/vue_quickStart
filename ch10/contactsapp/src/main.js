776d7208ee95ee448b93dfac11650f8f73a12bb8
dionysus0214 <hiba0214@naver.com>
2021-09-06 23:03:33 +0900

    연락처 애플리케이션 세팅(es6-promise, vuejs-paginate 설치 등)

--- ch10/contactsapp/src/main.js
+++ ch10/contactsapp/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './AppAxiosTest.vue'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+import ES6Promise from 'es6-promise'
+ES6Promise.polyfill()
 
-createApp(App).mount('#app')
+Vue.prototype.$axios = axios;
+
+createApp(App).use(axios).mount('#app')
